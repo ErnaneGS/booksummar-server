c1375020d2330566b1fca175342d8ad480086c87
Ernane Sousa <ernane.computacao@gmail.com>
2023-09-08 19:08:46 -0300

    Commit inicial: Projeto Java/Spring

---
+++ src/main/java/com/server/booksummar/BooksummarApplication.java
@@ -0,0 +1,13 @@
+package com.server.booksummar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksummarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksummarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/booksummar/BooksummarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.booksummar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksummarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
