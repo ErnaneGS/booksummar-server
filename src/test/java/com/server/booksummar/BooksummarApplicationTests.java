7f25211191b486e60882ac8eb4edabb7b7cbec0a
Ernane Sousa <ernane.computacao@gmail.com>
2023-10-14 17:11:16 -0300

    Adicionando dependencias e configurações

--- src/test/java/com/server/booksummar/BooksummarApplicationTests.java
+++ src/test/java/com/server/booksummar/BooksummarApplicationTests.java
@@ -1,13 +1,8 @@
 package com.server.booksummar;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BooksummarApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
