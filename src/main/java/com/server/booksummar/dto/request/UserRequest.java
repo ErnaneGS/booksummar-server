4694d001033208dee4a592ba24649394ef66c3f7
Ernane Sousa <ernane.computacao@gmail.com>
2023-10-24 18:12:34 -0300

    [BS-7] Refatorando Validações nas Requests

--- src/main/java/com/server/booksummar/dto/request/UserRequest.java
+++ src/main/java/com/server/booksummar/dto/request/UserRequest.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 public class UserRequest {
 
     @NotBlank(message = "O campo 'nome' não pode estar em branco.")
-    @Size(min = 3, max = 30)
+    @Size(min = 3, max = 30, message = "O campo 'nome' deve ter entre 3 e 30 caracteres")
     private String name;
 
     @NotBlank(message = "O campo 'email' não pode estar em branco.")
@@ -24,7 +24,8 @@ public class UserRequest {
 
     private String avatarURL;
 
-    @Size(min = 8, max = 20, message = "A senha deve ter entre 8 e 20 caracteres")
+    @NotBlank(message = "O campo 'senha' não pode estar em branco.")
+    @Size(min = 8, max = 20, message = "A 'senha' deve ter entre 8 e 20 caracteres")
     private String password;
 
 }
