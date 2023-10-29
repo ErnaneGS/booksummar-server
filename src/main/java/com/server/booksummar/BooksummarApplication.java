a73a9ec7a7b7f4f575996632171edb95b937cef6
Ernane Sousa <ernane.computacao@gmail.com>
2023-10-28 21:40:13 -0300

    [BS-10] Habilitando servidor de administração spring

--- src/main/java/com/server/booksummar/BooksummarApplication.java
+++ src/main/java/com/server/booksummar/BooksummarApplication.java
@@ -1,9 +1,11 @@
 package com.server.booksummar;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class BooksummarApplication {
 
 	public static void main(String[] args) {
