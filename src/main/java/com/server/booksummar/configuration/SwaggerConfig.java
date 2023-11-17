52404d080393b747c93af681c6d7b95aadd67e99
Ernane Sousa <ernane.computacao@gmail.com>
2023-11-17 14:06:24 -0300

    [NOJIRA] incluindo autenticação nas requisições Swagger

--- src/main/java/com/server/booksummar/configuration/SwaggerConfig.java
+++ src/main/java/com/server/booksummar/configuration/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.server.booksummar.configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,6 +28,16 @@ public class SwaggerConfig {
                 ).externalDocs(
                         new ExternalDocumentation()
                                 .description("Readme")
-                                .url("https://github.com/ErnaneGS/booksummar-server/tree/main"));
+                                .url("https://github.com/ErnaneGS/booksummar-server/tree/main"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+
     }
+
+    @SecurityScheme(
+            name = "bearerAuth",
+            type = SecuritySchemeType.HTTP,
+            scheme = "bearer",
+            bearerFormat = "JWT"
+    )
+    static class BearerAuth { }
 }
